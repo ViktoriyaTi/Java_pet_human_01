6ccc39c0963ebda7aa274bf25f596e6aaf9cb81d
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-12-22 12:35:26 +0300

    add tiger leo

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Human anna = new Human("Анна");
         Pet cat = new Pet("Мурзик");
-        anna.setCall(true);
+        anna.setCall(false);
         anna.call();
         cat.hearHuman(anna);
+        Tiges leo = new Tiges("Лео");
+        leo.hunt();
     }
 }
