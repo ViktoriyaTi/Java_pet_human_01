7a0df9380b671671f6730641070cc174df7aafa0
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-12-26 11:33:42 +0300

    add age

--- Animal.java
+++ Animal.java
@@ -1,8 +1,10 @@
 public abstract class Animal {
     private String name;
+    private int age;
 
-    public Animal(String name) {
+    public Animal(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     public String getName() {
@@ -13,7 +15,19 @@ public abstract class Animal {
         return this.name = name;
     }
 
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer setAge(int age) {
+        return this.age = age;
+    }
+
     abstract void walk();
 
     abstract void eat();
+
+    public String toString() {
+        return name + " " + "возраст:" + age;
+    }
 }
