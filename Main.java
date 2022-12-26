162fcf057e82117a6ea5e3261fb2501a6b609619
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-12-26 11:36:08 +0300

    add new cats,arraylist,sorting by age of cats

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
         Human anna = new Human("Анна");
-        Pet cat = new Pet("Мурзик");
+        Cats murzik = new Cats("Мурзик", 5);
+        Cats gosha = new Cats("Гоша", 7);
+        Cats pryanik = new Cats("Пряник", 12);
         anna.setCall(false);
         anna.call();
-        cat.hearHuman(anna);
-        Tiges leo = new Tiges("Лео");
+        murzik.hearHuman(anna);
+        Tiges leo = new Tiges("Лео", 10);
         leo.hunt();
+        ArrayList<Cats> listCats = new ArrayList<>();
+        listCats.add(murzik);
+        listCats.add(gosha);
+        listCats.add(pryanik);
+        Collections.sort(listCats, new AnimalAgeComparator());
+        System.out.println("Отсортированый список котов по возрасту(естественная сортировка): \n" + listCats);
     }
 }
