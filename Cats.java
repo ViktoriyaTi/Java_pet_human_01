0b303f368b39bc570775c8d65bffe5976d0c285d
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-12-26 11:33:13 +0300

    add age

---
+++ Cats.java
@@ -0,0 +1,27 @@
+
+public class Cats extends Animal {
+    public Cats(String name, Integer age) {
+        super(name, age);
+    }
+
+    public void hearHuman(Human human) {
+        if (human.getCall()) {
+            System.out.println("Питомец смотрит в сторону хозяина");
+            comeToHuman();
+        } else
+            walk();
+    }
+
+    public void comeToHuman() {
+        System.out.println("Питомец подошел к хозяину");
+    }
+
+    public void walk() {
+        System.out.println("Питомец гуляет");
+    }
+
+    public void eat() {
+        System.out.println("Питомец ест");
+    }
+
+}
